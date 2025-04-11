73a1f3cff7a059a0ae3dad942dd531ea2dd088c5
Mohammed Sadik <codeprogrammer111@gmail.com>
2025-04-11 10:25:54 +0300

    style: Add DropDownMenu component and integrate it into Navbar; update layout and styles in dashboard

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import Image from "next/image";
 import ShakeHand from "../../src/public/img/ShakeHand.png";
 import { Input } from "@/components/ui/input";
 import { LuSearch } from "react-icons/lu";
 import { GoBell } from "react-icons/go";
+import DropDownMenu from "./DropDownMenu";
 
 interface NavbarProps {
   name: string;
@@ -19,31 +22,39 @@ export default function Navbar({ name, time }: NavbarProps) {
   };
 
   return (
-    <div className="flex h-20 gap-4 items-center p-4 ml-2">
-      <div className="flex gap-3 ml-5">
-        <div>
-          <div className="flex">
-            <h1 className="text-lg font-semibold">Hello {name}</h1>
-            <Image
-              src={ShakeHand}
-              alt="Handshake icon"
-              width={18}
-              height={14}
-            />
+    <div className="flex h-20 w-full justify-between items-center p-4 ml-2">
+      <div className=" pl-5 " style={{paddingLeft: "5px"}} >
+        <div className="flex gap-3 ml-5">
+          <div>
+            <div className="flex gap-1">
+              <h1 className="text-lg font-semibold">Hello {name}</h1>
+              <Image
+                src={ShakeHand}
+                alt="Handshake icon"
+                width={18}
+                height={14}
+              />
+            </div>
+            <h3 className="text-sm text-gray-600">Good {getTimeOfDay()}</h3>
           </div>
-          <h3 className="text-sm text-gray-600">Good {getTimeOfDay()}</h3>
         </div>
       </div>
 
-      <div className="flex justify-center gap-2 border-1 border-gray-200 rounded-[8px] h-12 items-center p-2 ml-5 focus-visible:border-ring focus-visible:ring-ring focus-visible:ring-opacity-50 focus-visible:ring-1">
-        <LuSearch size={25} className="p-6 items-center" />
-        <div className="p-2">
-          <Input type="text" placeholder="Search" />
+      <div className="flex gap-4 w-1/2 justify-center items-center ">
+        <div className="flex justify-center gap-1 border-1 border-gray-200 rounded-[8px] h-12 items-center p-2 ml-5">
+          <LuSearch size={45} style={{padding: "10px"}}/>
+          <div>
+            <Input type="text" placeholder="Search" />
+          </div>
         </div>
-      </div>
 
-      <div className="flex w-12 h-12 bg-[#34495E1A] border-1 border-gray-200 rounded-[8px] justify-center items-center">
-        <GoBell size={20}/>
+        <div className="flex w-12 h-12 bg-[#34495E1A] border-1 border-gray-200 rounded-[8px] justify-center items-center">
+          <GoBell size={20} />
+        </div>
+
+        <div>
+          <DropDownMenu />
+        </div>
       </div>
     </div>
   );
